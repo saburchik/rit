72f7c4b93342e5e83d783e5097e965365de04dfb
Saburov <the.saburchik@gmail.com>
2022-08-05 17:15:53 +0700

    changed path bg-images and other

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,8 +7,8 @@ module.exports = {
           'linear-gradient(124deg, rgba(2,0,36,1) 0%, rgba(9,9,121,1) 35%, rgba(0,212,255,1) 100%)',
       },
       backgroundImage: {
-        starwars: "url('/assets/bg.jpg')",
-        swDetails: "url('/assets/details.jpg')",
+        starwars: "url('assets/bg.jpg')",
+        swDetails: "url('assets/details.jpg')",
       },
       boxShadow: {
         redSh: '2px 2px 4px rgb(200,67,54)',
